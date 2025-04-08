cf756ab571832e093af72842bde0766b70d9ae65
AKACHI-4 <adarshrawat8304@gmail.com>
2025-04-08 11:52:08 +0530

    feat: added newer disease model and pest model !!

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import io
 import uvicorn
 import pickle
 from fastapi.middleware.cors import CORSMiddleware
+from tensorflow.keras.applications.efficientnet_v2 import preprocess_input
+import os
 
 app = FastAPI()
 
@@ -17,28 +19,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define image constants
-IMG_HEIGHT, IMG_WIDTH = 224, 224
-
 # Load the CNN model (.h5 format)
 models = {
     "disease": tf.keras.models.load_model("models/disease.keras"),
+    "pest": tf.keras.models.load_model("models/pest.keras"),
 }
 
-# Load categories (labels) from a pickle file
 with open("models/disease.pkl", "rb") as f:
-    categories = pickle.load(f)
+    categories1 = pickle.load(f)
+with open("models/pest.pkl", "rb") as f:
+    categories2 = pickle.load(f)
 
-def preprocess_image(image):
+def preprocess_image1(image):
+    # Preprocess the image for MobileNetV2
+    IMG_HEIGHT, IMG_WIDTH = 224, 224
     image = image.convert("RGB")
     image = image.resize((IMG_HEIGHT, IMG_WIDTH))
-    image = np.array(image) / 255.0
+    image = np.array(image)
     image = np.expand_dims(image, axis=0)
     return image
+
+def preprocess_image2(image):
+    IMG_HEIGHT, IMG_WIDTH = 384, 384
+    image = image.convert("RGB")
+    image = image.resize((IMG_HEIGHT, IMG_WIDTH))
+    image = np.array(image)  # Convert PIL Image to NumPy array
+    image = preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+    return image
+
 
 @app.get("/")
 def home():
     return {"message": "Welcome to 100xFarmer Model Backend!"}
+
 
 @app.post("/predict/disease")
 async def predict(model_name: str = Form(...), file: UploadFile = File(...)):
@@ -46,11 +60,34 @@ async def predict(model_name: str = Form(...), file: UploadFile = File(...)):
         return {"error": "Model not found"}
     try:
         image = Image.open(io.BytesIO(await file.read()))
-        image = preprocess_image(image)
+        image = preprocess_image1(image)
         model = models[model_name]
         prediction = model.predict(image)
         predicted_class_index = int(np.argmax(prediction))
-        predicted_class_name = categories[predicted_class_index]
+        predicted_class_name = categories1[predicted_class_index]
+        confidence = float(np.max(prediction))
+        return {
+            "predicted_class_index": predicted_class_index,
+            "predicted_class_name": predicted_class_name,
+            "confidence": confidence
+        }
+    except Exception as e:
+        return {"error": f"Failed to process image: {str(e)}"}
+
+
+@app.post("/predict/pest")
+async def predict(model_name: str = Form(...), file: UploadFile = File(...)):
+    if model_name not in models:
+        return {"error": "Model not found"}
+    try:
+        image = Image.open(io.BytesIO(await file.read()))
+        image = preprocess_image2(image)
+        model = models[model_name]
+        prediction = model.predict(image)
+        # print(f"Prediction: {prediction}")
+        predicted_class_index = int(np.argmax(prediction))
+        # print(f"Predicted class index: {predicted_class_index}")
+        predicted_class_name = categories2[predicted_class_index]
         confidence = float(np.max(prediction))
         return {
             "predicted_class_index": predicted_class_index,
